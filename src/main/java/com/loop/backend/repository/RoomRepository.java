39ba15ccbad39979c31e52ee23872b9c514267e2
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-02 18:10:24 +0530

    replaced JpaRepository with MongoRepository in RoomRepository interface

--- src/main/java/com/loop/backend/repository/RoomRepository.java
+++ src/main/java/com/loop/backend/repository/RoomRepository.java
@@ -1,14 +1,13 @@
 package com.loop.backend.repository;
 
 import com.loop.backend.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, UUID> {
+public interface RoomRepository extends MongoRepository<Room, String> {
 
     Optional<Room> findByRoomId(String roomId);
 }
